d4e80245e7778060ee6638c1939b9a12277768f1
LonelyOwl0 <medfouad2000@gmail.com>
2023-03-20 09:17:37 +0200

    Entire SignUp, Login, And reset password part, CSS is still messed up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { environment } from '../environments/environment';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { ResetPasswordComponent } from './reset-password/reset-password.component';
-import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-
-
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+import {environment} from '../environments/environment';
+import {LoginComponent} from './login/login.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
+import {ResetPasswordComponent} from './reset-password/reset-password.component';
+import {FormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+import {UserProfileComponent} from './user-profile/user-profile.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {AngularFireAuthGuard, redirectUnauthorizedTo} from '@angular/fire/compat/auth-guard';
 
 
 const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'sign-up', component: SignUpComponent },
-  { path: 'reset-password', component: ResetPasswordComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'reset-password', component: ResetPasswordComponent },
-  { path: 'user-profile', component: UserProfileComponent },
+  {path: 'login', component: LoginComponent},
+  {path: 'sign-up', component: SignUpComponent},
+  {path: 'reset-password', component: ResetPasswordComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'reset-password', component: ResetPasswordComponent},
+  {
+    path: 'user-profile',
+    component: UserProfileComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: {authGuardPipe: () => redirectUnauthorizedTo(['/login'])}
+  },
+
 
 ];
-
-
 
 
 @NgModule({
@@ -49,4 +52,5 @@ const appRoutes: Routes = [
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-user-profile',
@@ -9,12 +10,24 @@ import { AuthService } from '../auth.service';
 export class UserProfileComponent implements OnInit {
   user: any;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
     this.authService.getUser().subscribe(user => {
       this.user = user;
     });
   }
 
+
+
   ngOnInit(): void {
   }
+
+  async disconnect() {
+    try {
+      await this.authService.signOut();
+      this.router.navigate(['/login']);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
 }
